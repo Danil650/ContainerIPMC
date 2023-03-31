2f8213385a1a77228b7480f59f57327bf34a1214
danil idrisov <32590763+Danil650@users.noreply.github.com>
2023-03-31 18:10:49 +0500

    Add files via upload

--- src/pages/api/uploudedata/import.ts
+++ src/pages/api/uploudedata/import.ts
@@ -1,13 +1,13 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import ExcelData from "../../../../lib/ExcelData"
 import Container from "../../../../lib/Container"
 import Substance from "../../../../lib/Substance"
 import SubstCont from "../../../../lib/SubstContainer"
-import { query } from '../../../../lib/db'
+import { insertSubstances } from '../../../../lib/db'
 import uuid from 'react-uuid';
 import { from } from 'linq-to-typescript'
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "POST") {
         const data = req.body as ExcelData[];
         if (data[0].CAS === "CAS" && data[0].Container === "Коробка" &&
@@ -22,7 +22,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         let uplouded: Container;
         let Substance: Substance[] = [];
         let SubstCont: SubstCont[] = [];
-        data.forEach(async element => {
+        data.forEach( element => {
             if (element.Id != 0 && element.Container) {
                 let cont: Container = {
                     Id: `${element.Id}${element.Container}`,
@@ -45,65 +45,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                     URL: element.URL
                 }
                 Substance.push(subst);
-
                 let substcont: SubstCont = {
                     Id: uuid(),
                     ContId: from(Containers).where(x => x.ExcelId == element.Id).select(x => x.Id).first(),
                     SubstId: subst.Id
                 }
                 SubstCont.push(substcont);
-                try {
-                    await query("INSERT INTO containerdb.substance (Id, SubstName, CAS, Meaning, Mass, Formula, Investigated, `Left`, URL) " +
-                        "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?); ",
-                        [subst.Id, subst.SubstName as string, subst.CAS as string, subst.Meaning as string, subst.Mass as string, subst.Formula as string, subst.Investigated, subst.Left, subst.URL as string]);
-                } catch (error) {
-                    console.log(error);
-                }
+            }
+        });
 
-            }
-        });
+        insertSubstances(Substance,Containers,SubstCont);
+
         //добавление контейнеров без повторений
-        Containers.forEach(async element => {
-            if (uplouded) {
-                if (uplouded.ExcelId == element.ExcelId &&
-                    uplouded.ContainsIn == element.ContainsIn &&
-                    uplouded.Name == element.Name) {
-                    uplouded = element;
-                }
-                else {
-                    uplouded = element;
-                    if(element.ExcelId)
-                     try {
-                    await query("INSERT INTO containerdb.contwthcont (Id, ExcelId, ContainsIn, Name) VALUES"+
-                     "(?, ?, ?, ?);",
-                        [element.Id, element.ExcelId, from(Containers).where(x=>x.ExcelId == element.ContainsIn).select(x=>x.Id).firstOrDefault() ?? '' , element.Name]);
-                } catch (error) {
-                    console.log(error);
-                }
-                }
-            }
-            else {
-                uplouded = element;
-                try {
-                    await query("INSERT INTO containerdb.contwthcont (Id, ExcelId, ContainsIn, Name) VALUES"+
-                     "(?, ?, ?, ?);",
-                        [element.Id, element.ExcelId ?? 0, element.ContainsIn ?? '' , element.Name]);
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-        });
-         SubstCont.forEach(async element => {
-            try {
-                await query("INSERT INTO containerdb.substcont (Id, SubstId, ContId) VALUES (?, ?, ?); ",
-                    [element.Id,element.SubstId,element.ContId ]);
-            } catch (error) {
-                console.log(`INSERT INTO containerdb.substcont (Id, SubstId, ContId) VALUES (${element.Id}, ${element.SubstId}, ${element.ContId}); `)
-                console.log(error);
-                console.log(element.Id,element.SubstId,element.ContId )
-                console.log(from(Substance).where(x=>x.Id == element.SubstId).toArray());
-            }
-        });
+         
         res.status(200).json({ message: "Данные импортированы" });
     }
     else {
